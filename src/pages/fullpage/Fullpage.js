124befe03914d8ed73f4ba29de8408e08c7bb06e
Mario <grinderfx@gmail.com>
2020-04-03 08:12:35 +0000

    new components

---
+++ src/pages/fullpage/Fullpage.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+//import Babylon from '../Babylon';
+//import './header.scss';
+
+function Fullpage() {
+  
+  useEffect(() => {
+    const bab = new Babylon(100, 100);
+  });
+  return (
+    <div className="header">
+        <canvas id="renderCanvas" touch-action="none"></canvas>
+    </div>
+  );
+}
+
+export default Fullpage;
